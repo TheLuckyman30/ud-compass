35f067396e327006c6c63a99c2915193c8b4851e
TheLuckyman30 <bnauta0@gmail.com>
2026-05-07 02:55:13 -0400

    Adjusted layout and style of item info

--- src/components/ItemInfo.tsx
+++ src/components/ItemInfo.tsx
@@ -6,7 +6,7 @@ interface ItemInfoProps {
 
 export function ItemInfo({ item }: ItemInfoProps) {
   return (
-    <div className="bg-white rounded-sm border border-gray-400 px-5 py-10 shadow-md w-[75%]">
+    <div className="bg-white rounded-sm border border-gray-400 px-5 py-10 shadow-md">
       <p className="font-bold text-2xl">{item.title}</p>
       <p className="text-sm text-gray italic">{item.subtitle}</p>
       <br />
--- src/pages/resource/Resource.tsx
+++ src/pages/resource/Resource.tsx
@@ -3,9 +3,16 @@ import { useGeneralStoe } from "../../utils/zustand/general-store";
 
 export function Resoruce() {
   const selectedItem = useGeneralStoe((state) => state.selectedItem);
+  const setCurrentPage = useGeneralStoe((state) => state.setCurrentPage);
 
   return (
-    <div className="flex w-full justify-center">
+    <div className="flex flex-col max-w-7xl mx-auto gap-5">
+      <button
+        className="p-2 text-xs bg-ud-darkBlue rounded-md text-white font-medium shadow-md cursor-pointer hover:scale-105 duration-75 w-fit"
+        onClick={() => setCurrentPage(1)}
+      >
+        Go Back
+      </button>
       <ItemInfo item={selectedItem} />
     </div>
   );
