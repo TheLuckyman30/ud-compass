509e5e109539d8c36bb082fd3d738fbcee09458e
TheLuckyman30 <bnauta0@gmail.com>
2026-05-15 05:26:59 -0400

    Added zustand state vars to manage favorite items

--- src/utils/zustand/general-store.ts
+++ src/utils/zustand/general-store.ts
@@ -1,18 +1,21 @@
 import { create } from "zustand";
 import { buildData } from "@utils/helpers";
 import { PAGES } from "@utils/constants";
-import type { ItemMap, CategoryMap } from "@utils/interfaces";
+import type { ItemMap, CategoryMap, ItemId, Item } from "@utils/interfaces";
 import type { JSX } from "react";
 
 interface GeneralStore {
   currentPage: () => JSX.Element | null;
   allCategories: CategoryMap;
   allItems: ItemMap;
+  favoriteItems: ItemMap;
   selectedCategoryId: string;
   selectedItemId: string;
   setCurrentPage: (newPage: () => JSX.Element | null) => void;
   setSelectedCategory: (newCategoryId: string) => void;
   setSelectedItem: (newItemId: string) => void;
+  addFavItem: (itemId: ItemId, item: Item) => void;
+  removeFavItem: (itemId: ItemId) => void;
 }
 
 export const useGeneralStore = create<GeneralStore>((set) => {
@@ -22,6 +25,7 @@ export const useGeneralStore = create<GeneralStore>((set) => {
     currentPage: PAGES.Home,
     allCategories: categoryMap,
     allItems: itemMap,
+    favoriteItems: new Map<ItemId, Item>(),
     selectedCategoryId: "cat-0",
     selectedItemId: "item-0",
     setCurrentPage: (newPage: () => JSX.Element | null) => {
@@ -32,6 +36,22 @@ export const useGeneralStore = create<GeneralStore>((set) => {
     },
     setSelectedItem: (newItemId: string) => {
       set({ selectedItemId: newItemId });
+    },
+    addFavItem: (itemId: ItemId, item: Item) => {
+      set((state) => {
+        const newFavoriteItems = new Map(state.favoriteItems);
+        newFavoriteItems.set(itemId, item);
+
+        return { favoriteItems: newFavoriteItems };
+      });
+    },
+    removeFavItem: (itemId: ItemId) => {
+      set((state) => {
+        const newFavoriteItems = new Map(state.favoriteItems);
+        newFavoriteItems.delete(itemId);
+
+        return { favoriteItems: newFavoriteItems };
+      });
     },
   };
 });
