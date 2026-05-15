fa1aa381aa4d937ea7244135cc754ecb295bc796
TheLuckyman30 <bnauta0@gmail.com>
2026-05-15 03:57:55 -0400

    Moved css into css files

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,13 @@
 import { CategoryCard } from "@components";
 import resources from "@data/resources.json";
+import "@css/pages/home.css";
 
 export function Home() {
   return (
-    <section className="flex flex-col gap-10">
-      <div className="flex flex-col gap-5">
-        <p className="font-bold text-3xl text-gray">Categories</p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
+    <section className="home">
+      <div className="h-cat-container">
+        <p className="h-cat-header">Categories</p>
+        <div className="h-categories">
           {resources.map((resource) => (
             <CategoryCard category={resource} />
           ))}
--- src/pages/resource/Resource.tsx
+++ src/pages/resource/Resource.tsx
@@ -1,18 +1,19 @@
 import { ItemInfo } from "@components";
 import { PAGES } from "@utils/constants";
 import { useGeneralStoe } from "@utils/zustand";
+import "@css/pages/resource.css";
 
 export function Resource() {
   const selectedItem = useGeneralStoe((state) => state.selectedItem);
   const setCurrentPage = useGeneralStoe((state) => state.setCurrentPage);
 
   return (
-    <div className="flex flex-col max-w-7xl mx-auto gap-5">
+    <div className="resource">
       <button
-        className="p-2 text-xs bg-ud-darkBlue rounded-md text-white font-medium shadow-md cursor-pointer hover:scale-105 duration-75 w-fit"
+        className="res-back"
         onClick={() => setCurrentPage(PAGES.Category)}
       >
-        Go Back
+        ← Go Back
       </button>
       <ItemInfo item={selectedItem} />
     </div>
